553bfc2faaf61f4aa42909a41d1a2f00d34cf3fd
Chad Zhuang <changda0616@gmail.com>
2017-10-22 16:27:15 +0800

    supersquad data set

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from "react-dom";
+import App from './component/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+
+import rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer);
+console.log('store.getstate()', store.getState())
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,document.getElementById('root'));
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import characters_json from '../data/characters.json';
+
+function characters(state = characters_json, action) {
+    switch (action.type) {
+        // case value:
+            
+        //     break;
+    
+        default:
+            return state;
+    }    
+}
+export default characters;
